42a5e930fa870d7b3910b099d1eec616dca27493
kyoncy <kyota.n.2014@gmail.com>
2021-01-08 13:33:20 +0900

    add send arguments (#2)

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ try {
   const webhookUrl = core.getInput('webhook');
   const webhook = new IncomingWebhook(webhookUrl);
 
+  const userName = core.getInput('slack_username');
+  const iconUrl = core.getInput('slack_icon_url');
+  const color = core.getInput('slack_color');
+  const title = core.getInput('slack_title');
+
   webhook.send({
-    text: 'slack message action test',
+    username: userName,
+    icon_url: iconUrl,
+    attachments: [{
+      color: color,
+      fields: [{
+        title: title,
+        value: 'TODO: commit message area',
+      }]
+    }]
   }).catch(error => {
     core.setFailed(error.message);
   })
